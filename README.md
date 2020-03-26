7d7b05e6c2627d1083459a6a7cb71e549dc0e011
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-03-26 07:11:22 +0200

    fit for all bands. separate rms construction. math updates for band adjuster

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Lu163_WobblingEnergies_March2020
 Calculating the wobbling spectra of the ${163}$Lu wobbler. Within 2017 PRC formalism (corrected by 2018JPG)
+
+## Potential results
+Using the search with separate RMS values for each band:
+
+* PARAMETERS:
+  * 51.57 0.07  
+* Separate RMS values:  
+  * 0.468175 0.454279 0.333575 1.02723  
+  * s= 3.6099  
+* E_RMS =0.570814  
+* Process took... 136.611 s
+
+![Image1](/sources/2020-03-25-10-27-49.png)  
+*the results for the fit*
+
+## Results for decrease in step size
+
+The order of $V_\text{step}\approx0.001$
+PARAMETERS:  
+51.56 0.052  
+Separate RMS values:  
+0.468278 0.45827 0.340529 1.00261  
+s= 2.68112  
+E_RMS =0.567422  
+Process took... 622.215 s
+
+![Image2](/sources/2020-03-25-17-03-15.png)  
+*the results for the fit*
+
+## Results for smaller steps
+
+
+The order of $V_\text{step}\approx0.0001$
+The order of $\mathcal{I}_\text{step}\approx0.01$
+PARAMETERS:  
+51.56 0.0307  
+Separate RMS values:  
+0.468411 0.463288 0.349418 0.972099  
+s= 1.58289  
+E_RMS =0.563304  
+Process took... 6419.45 s  
--- include/fitData.h
+++ include/fitData.h
@@ -16,18 +16,19 @@ public:
 public:
     struct paramLimits
     {
-        const double I0_left = 48;
-        const double I0_right = 58;
-        const double I0_step = 0.01;
-        const double V_left = 0.1;
-        const double V_right = 10;
-        const double V_step = 0.01;
+        const double I0_left = 50;
+        const double I0_right = 52;
+        const double I0_step = 0.001;
+        const double V_left = 0.0001;
+        const double V_right = 3;
+        const double V_step = 0.0001;
     };
     struct paramSet
     {
         double I0;
         double V;
         double E_RMS;
+        double E_RMS1, E_RMS2, E_RMS3, E_RMS4;
     };
 
     //methods for calculating the RMS
@@ -37,6 +38,11 @@ public:
     //method to construct the theoretical wobbling excitation container
     static void GenerateTheoreticalEnergies(double i0, double particlePotential, double beta, double gamma, std::vector<double> &energies);
     static void SearchMinimum_RMS(ExperimentalData &obj, FitData::paramSet &bestParams, double beta, double gamma);
+    //search the parameters by getting separate rms for each band
+    static void SearchRMS_SeparateBands(FitData::paramSet &bestParams, double beta, double gamma);
+    //generates the theoretical band separately (based on each of the experimental ones)
+    static std::vector<double> GenerateTheoreticalBand(int bandIndex, std::vector<double> &inputSpins, double iZero, double particlePotential, double beta, double gamma);
+    static double AverageRMS(FitData::paramSet &params);
 };
 
 #endif // FITDATA_HH
